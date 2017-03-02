1c4bd8142805bf179680cb87d969efa7b4d8d759
Allan <allanzi.santos@gmail.com>
2017-03-02 00:28:48 -0300

    Corrigidos Bugs de responsividade e atualizada skills

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
     var height = $(window).width();
     var navHeight = $('.nav').height();
-    
-//    $('.at-at').css({'top': navHeight});
+
     $('body').css({'with': height});
 });
